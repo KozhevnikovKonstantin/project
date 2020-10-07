7d8f2025698a202af7d13216cacb9f8c9c411e4e
Konstantin <kozhevnikovkons@gmail.com>
2020-10-07 21:05:16 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+//alert('I rule the ruins');
+
+//const result = confirm("Are you fucking crazy?!!");
+
+//const answer = prompt("Who the hell you are?","Idiot?");
+
+//alert('The world is mine!');
+
+//const category = 'toys';
+//console.log(`https://someurl.com/${category}/5`);
+
+//const user = 'Андрей';
+//alert(`Привет, ${user}`);
+
+const isChecked = true;
+const isClose = false;
+console.log(isChecked || isClose);
+
